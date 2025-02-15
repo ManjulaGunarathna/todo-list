26b274d1b395d4075d3ec7db7d891ea7faed03fc
manjulagunarathna <manjulagunarathna500@gmail.com>
2025-02-15 11:56:05 +0530

    Create connection with mysql database

---
+++ db.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "localhost";   // Your database server (typically 'localhost')
+$username = "root";          // Your database username (default 'root' for XAMPP)
+$password = "";              // Your database password (default is empty for XAMPP)
+$dbname = "todo_app";        // The name of the database you want to connect to
+
+// Create connection
+$conn = new mysqli($servername, $username,$password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    // If there is an error with the connection, display it
+    die("Connection failed: " . $conn->connect_error);
+}else{
+// If connection is successful
+echo "Connected successfully to the database!";
+}
+?>
